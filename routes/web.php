37857bd0e2bc713ec1f0ac38a5e797eb905151fb
nouglozeh <54663785+nouglozeh@users.noreply.github.com>
2019-08-29 09:37:26 +0000

    good

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
 //jiv est bien présent bla bla
-=======
-//jiv est bien présent tot
->>>>>>> 410511c586c2cee7d68abb03cbede7b3df4beaee
 /*
 |--------------------------------------------------------------------------
 | Web Routes
